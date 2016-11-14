2ee08b8432afc3984ab982d88d47df0b4de13017
Jabran Shakil <jabranshakil@live.com>
2016-11-14 13:13:33 -0600

    Made jQuery no longer control the $ variable

--- pp-08/site.js
+++ pp-08/site.js
@@ -1,14 +1,17 @@
+$.noConflict();
+(function($) {
 $(document).ready(
-  function() {
-    $('html').removeClass('nojs');
-    $('html').addClass('hasjs');
+    function () {
+        $('html').removeClass('nojs');
+        $('html').addClass('hasjs');
 
-    if (typeof(window.removeClass) !== "undefined") {
-      console.log('jQuery still controls the $ variable');
-    } else {
-      console.log('Nice work! jQuery no longer controls the $ variable');
+        if (typeof (window.$) !== "undefined") {
+            console.log('jQuery still controls the $ variable');
+        } else {
+            console.log('Nice work! jQuery no longer controls the $ variable');
+        }
+
     }
-
-  }
 );
+})(jQuery);
 
